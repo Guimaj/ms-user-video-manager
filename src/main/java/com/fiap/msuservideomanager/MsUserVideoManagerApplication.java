7615b7931826e1dcd453a198d01ec49dfdf11d29
= <=>
2025-02-04 19:37:52 -0300

    first commit

---
+++ src/main/java/com/fiap/msuservideomanager/MsUserVideoManagerApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.msuservideomanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsUserVideoManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsUserVideoManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/https/github/com/Guimaj/ms_user_video_manager/git/ms_user_video_manager/MsUserVideoManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package https.github.com.Guimaj.ms_user_video_manager.git.ms_user_video_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUserVideoManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
