e6356e1264481c10eba1b462546d688916ee27a3
Guimaj <deco-jorge@hotmail.com>
2025-02-09 19:00:33 -0300

    removendo @data

--- src/main/java/com/fiap/msuservideomanager/entrypoint/dto/UrlDTO.java
+++ src/main/java/com/fiap/msuservideomanager/entrypoint/dto/UrlDTO.java
@@ -2,14 +2,28 @@ package com.fiap.msuservideomanager.entrypoint.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UrlDTO {
     private String url;
     @JsonProperty("x-amz-meta-arquivo-id")
     private String idArquivo;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getIdArquivo() {
+        return idArquivo;
+    }
+
+    public void setIdArquivo(String idArquivo) {
+        this.idArquivo = idArquivo;
+    }
 }
