61bc542a2cdfdecdf3c44e595d76906ac9f2f167
Guimaj <deco-jorge@hotmail.com>
2025-02-09 19:08:10 -0300

    adicionando annotation getter e setter

--- src/main/java/com/fiap/msuservideomanager/entrypoint/dto/ArquivoDTO.java
+++ src/main/java/com/fiap/msuservideomanager/entrypoint/dto/ArquivoDTO.java
@@ -1,45 +1,17 @@
 package com.fiap.msuservideomanager.entrypoint.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 public class ArquivoDTO {
     private String nome;
     private String tipo;
     private String tamanho;
     private String intervalo;
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
-
-    public String getTamanho() {
-        return tamanho;
-    }
-
-    public void setTamanho(String tamanho) {
-        this.tamanho = tamanho;
-    }
-
-    public String getIntervalo() {
-        return intervalo;
-    }
-
-    public void setIntervalo(String intervalo) {
-        this.intervalo = intervalo;
-    }
 }
